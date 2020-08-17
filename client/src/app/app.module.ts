16323ea005598cc36b95b390d90d2a2cede27f31
Jose Pinto <pintojose.benedicto2@gmail.com>
2020-08-17 04:30:01 -0600

    Created nav bar and basic ui layout. Also started adding models

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,21 @@
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { AppComponent } from './components/app/app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavBarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
