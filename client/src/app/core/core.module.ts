27bac15d4698aab56464cf89325a39d3a1eebc97
Jose Pinto <pintojose.benedicto2@gmail.com>
2020-08-20 06:27:04 -0600

    Added client error handling

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
+  ],
+  declarations: [ServerErrorComponent, NotFoundComponent],
 })
 export class CoreModule {}
